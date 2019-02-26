c0c53199b4f2ecab88e865e709c46ce67af9013b
Martaalex <marta.aleksandrovic@gmail.com>
2019-02-26 21:46:15 +0200

    change language

--- src/app/components/header/index.js
+++ src/app/components/header/index.js
@@ -1,34 +1,50 @@
 import React from 'react';
 import './index.scss';
-
+import PropTypes from 'prop-types';
 import data from './data';
 
-function Header() {
+function Header(props) {
+  const { language, onLanguage } = props;
+  const { contacts, pic, name, summary } = data[language];
   return (
     <header className="Header">
-      <h1>{data.name}</h1>
+      <h1>{name}</h1>
       <hr />
       <p className="Header--contact">
-        {`${data.contacts.phone.label} ${data.contacts.phone.value}`}
+        {`${contacts.phone.label} ${contacts.phone.value}`}
       </p>
       <p className="Header--contact">
-        {`${data.contacts.email.label}`}
+        {`${contacts.email.label}`}
         <a
-          href={`mailto:${data.contacts.email.value}`}
+          href={`mailto:${contacts.email.value}`}
           target="_blank"
           rel="noopener noreferrer"
         >
-          {`${data.contacts.email.value}`}
+          {`${contacts.email.value}`}
         </a>
       </p>
-      <p className="Header--summary">{data.summary}</p>
-      <img
-        className="Header--profile-pic"
-        alt={data.pic.alt}
-        src={data.pic.src}
-      />
+      <p className="Header--summary">{summary}</p>
+      <img className="Header--profile-pic" alt={pic.alt} src={pic.src} />
+      <select
+        onChange={e => onLanguage(e.target.value)}
+        className="Header--language-select"
+      >
+        <option value="en">English</option>
+        <option value="lt">Lithuanian</option>
+        <option value="pl">Polish</option>
+        <option value="ru">Russian</option>
+      </select>
     </header>
   );
 }
+
+Header.propTypes = {
+  language: PropTypes.oneOf(['en', 'lt', 'pl', 'ru']),
+  onLanguage: PropTypes.func.isRequired,
+};
+
+Header.defaultProps = {
+  language: 'en',
+};
 
 export default Header;
--- src/app/index.js
+++ src/app/index.js
@@ -4,21 +4,35 @@ import { Header, Main, Footer } from './components';
 // import Footer from './components/footer';
 import './index.scss';
 
-function App() {
-  return (
-    <React.Fragment>
-      <div className="App">
-        <Header />
-        <Main />
-        <select className="App--language-select">
-          <option>English</option>
-          <option>Polish</option>
-          <option>Russian</option>
-        </select>
-      </div>
-      <Footer />
-    </React.Fragment>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      language: 'en',
+    };
+  }
+
+  render() {
+    const { language } = this.state;
+    return (
+      <React.Fragment>
+        <div className="App">
+          <Header onLanguage={lang => this.setState({ language: lang })} />
+          <Main language={language} />
+          {/* <select
+            onChange={e => this.setState({ language: e.target.value })}
+            className="App--language-select"
+          >
+            <option value="en">English</option>
+            <option value="lt">Lithuanian</option>
+            <option value="pl">Polish</option>
+            <option value="ru">Russian</option>
+          </select> */}
+        </div>
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
